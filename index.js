470210fa9046e40ae40efc96dcdba654538e54de
Dencyh <a.dencyh@gmail.com>
2021-11-24 19:09:23 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+const XLSX = require('xlsx')
+const fs = require('fs')
+
+// Get file names
+const folderPath = './Put_original_files_here'
+const filesList = []
+
+fs.readdirSync(folderPath).forEach(file => {
+    filesList.push(file)
+});
+//
+// Cell names
+
+
+// Лист Маршруты
+const cellWarehouse = 'F2'
+
+//Лист Расширенные маршруты
+const cellDate = 'K3'
+
+// Лист Метрики
+const cellCouriersCount = 'C2'
+const cellOrdersCount = 'C3'
+const cellSumRouteLength = 'C10'
+
+let resultArray = []
+
+// Sheets' names
+let SimpleRoutes = []
+let ExtendedRoutes = []
+let Metrics = []
+
+
+
+for (let i = 0; i < filesList.length; i++) {
+    const singleFile = XLSX.readFile(`${folderPath}/${filesList[i]}`, { cellDates: true })
+    // Getting Sheet's data from single file
+    SimpleRoutes = singleFile.Sheets['Маршруты']
+    ExtendedRoutes = singleFile.Sheets['Расширенные маршруты']
+    Metrics = (singleFile.Sheets['Метрики']) /* XLSX.utils.sheet_to_json */
+    SimpleRoutesJSON = XLSX.utils.sheet_to_json(SimpleRoutes)
+
+    if ((SimpleRoutes && ExtendedRoutes && Metrics)) {
+        console.log(filesList[i])
+        return
+    }
+
+    // Creating AoA
+    const oneDayData = []
+
+    // Warehouse column
+    const warehouseData = SimpleRoutes[cellWarehouse].v
+
+    // Date coulumn (first 10 symbols )
+    const date = ExtendedRoutes[cellDate].v.slice(0, 10)
+
+    // Couriers column
+    const couriersCount = Metrics[cellCouriersCount].v
+
+    // Addresses column
+    const addressArray = []
+    SimpleRoutesJSON.forEach((address) => {
+        if (address['Адрес']) {
+            addressArray.push(address['Адрес'])
+        }
+
+    })
+    const uniqueAddresses = [...new Set(addressArray)].length
+
+    // Orders column
+    const ordersCount = Metrics[cellOrdersCount].v
+
+    // Lots count column
+    const lotArray = []
+    SimpleRoutesJSON.forEach((lotName) => {
+        if (lotName['Комментарий']) {
+            lotArray.push(lotName['Комментарий'])
+        }
+
+    })
+    const uniqueLots = [...new Set(lotArray)].length
+
+
+    // Travel distance culumn
+    const travelDistanceAvergae = (Metrics[cellSumRouteLength].v / couriersCount).toFixed(1)
+
+
+    oneDayData.push(warehouseData, date, couriersCount, uniqueAddresses, ordersCount, uniqueLots, travelDistanceAvergae)
+
+    // Pushing one day data as an array
+    resultArray.push(oneDayData)
+
+}
+resultArray.unshift(['Склад', 'Дата', 'Количество курьеров', 'Количество точек', 'Количество заказов', 'Количество лотов', 'Пробег среднее'])
+console.log(resultArray)
+
+const newWS_name = 'Results'
+const newWS = XLSX.utils.aoa_to_sheet(resultArray, { wpx: 200 })
+newWS['!cols'] = [{ wpx: 85 }, { wpx: 80 }, { wpx: 120 }, { wpx: 100 }, { wpx: 110 }, { wpx: 100 }, { wpx: 90 }]
+const newWB = XLSX.utils.book_new()
+
+XLSX.utils.book_append_sheet(newWB, newWS, newWS_name)
+XLSX.writeFile(newWB, './Result.xlsx')
